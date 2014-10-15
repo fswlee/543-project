8c60d8fac7a1ac2da54a0ce6d89643cebac8f361
HBrookhart <brookhar@msu.edu>
2014-10-14 21:20:40 -0400

    update ClientInformation.java and xml

--- BusinessPlan/app/src/main/java/com/edu/umich/businessplan/businessplan/ClientInformation.java
+++ BusinessPlan/app/src/main/java/com/edu/umich/businessplan/businessplan/ClientInformation.java
@@ -15,7 +15,20 @@ public class ClientInformation extends Activity {
     }
 
 
-//pseudo code goes here//
+//display textView1, textView2, textView3, textView4, editText1, editText2, editText3, editText4,
+//imageButton1, imageButton2, imageButton3, and imageButton4
+
+  //activate keyboard if the user selects any of the editText objects
+
+  //if the user touches any of the image buttons in the first horizantal linear layout, the button
+  //goes from unselected. only one of the three buttons can be selected at one time
+
+  //if the user selected the forward button, an onClick event is triggered
+
+    //the algorithm that generated the You and Your Community Graph is updated to include
+    //information from questions 2, 3, and 4
+
+    //activity 3 (BusinessInformation) loads
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
