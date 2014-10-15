4456cb612fb1fcbb8a2480ae2f87bd4f292c9725
HBrookhart <brookhar@msu.edu>
2014-10-14 20:44:43 -0400

    updated NewPlan.java and activity_new_plan.xml

--- BusinessPlan/app/src/main/java/com/edu/umich/businessplan/businessplan/NewPlan.java
+++ BusinessPlan/app/src/main/java/com/edu/umich/businessplan/businessplan/NewPlan.java
@@ -8,16 +8,20 @@ import android.view.View;
 
 public class NewPlan extends Activity {
 
+    //display textView1//
+    //display button1//
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_plan);
     }
 
+    //when user selects button1, load activity 2 (ClientInformation)//
     public void openActivity(View view) {
         Intent intent = new Intent(getApplicationContext(), ClientInformation.class);
         startActivity(intent);
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
