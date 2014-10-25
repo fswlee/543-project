bf3fdbc6baaf867b54219b500873bb259647978b
jgjwolf <jgjwolf@umich.edu>
2014-10-25 15:57:44 -0400

    Pseudo code for Activity SuggestionsForImprovement

--- BusinessPlan/app/src/main/java/com/edu/umich/businessplan/businessplan/SuggestionsForImprovement.java
+++ BusinessPlan/app/src/main/java/com/edu/umich/businessplan/businessplan/SuggestionsForImprovement.java
@@ -12,11 +12,53 @@ import android.view.View;
 
 //upon instantiation, display textView
     //from the suggestions algorithm, display suggestions as checkBox
-    //from Customer Recommendations
-        //if array rec_customer > 0
-            //print "You should sell to" array rec customer" pseudo code pseudo code pseudo code
-        //if rec_customer =0
-            //exit
+    /*These are the recommendations overall
+
+
+//if array rec_customer > 0
+                //print "You should sell to" array rec customer" pseudo code pseudo code pseudo code
+            //if rec_customer =0
+                //exit
+
+    Improvement plan for YourHours
+    for day in list_Morning ()
+    if day !=0
+        morningTotal+=1
+    if morningTotal <= 4
+        print "We recommend that you are open more mornings."
+    for day in list_Morning(5:6)
+        if day != 0,
+            weekendTotal +=1
+    for day in list_Morning (0:4)
+        if day !=0,
+            weekdayTotal+=1
+    for day in list_Evening ()
+        if day !=0
+            eveningTotal+=1
+    if eveningTotal <= 4
+        print "We recommend that you are open more evenings."
+    for day in list_Evening(5:6)
+        if day != 0,
+            weekendTotal +=1
+    for day in list_Evening (0:4)
+        if day !=0,
+            weekdayTotal+=1
+    for day in list_Afternoon ()
+        if day !=0
+            afternoonTotal+=1
+    if afternoonTotal <= 4
+        print "We recommend that you are open more afternoons."
+    for day in list_Afternoon(5:6)
+        if day != 0,
+            weekendTotal +=1
+    for day in list_Afternoon (0:4)
+        if day !=0,
+            weekdayTotal+=1
+    if weekendTotal <=3
+        print "We recommend that you are open more on the weekends."
+    if weekdayTotal <=8
+        print "We recommend that you are open more weekdays."
+        */
 
     //allow the user to select no more than five checkBox
 
