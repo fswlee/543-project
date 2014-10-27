75878912135e922246fb2f407de214b90a9bac97
HBrookhart <brookhar@msu.edu>
2014-10-27 00:04:27 -0400

    linked action bar items w/ activities
    First three action bar items only
    -declared methods in BaseActivity.java
    -changed onOptionsItemSelected to recognize clicks on those items

--- BusinessPlan/app/src/main/java/com/edu/umich/businessplan/businessplan/BaseActivity.java
+++ BusinessPlan/app/src/main/java/com/edu/umich/businessplan/businessplan/BaseActivity.java
@@ -1,12 +1,17 @@
 package com.edu.umich.businessplan.businessplan;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
+
 public class BaseActivity extends Activity {
+
+    private final String TAG = "Base Activity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,15 +27,46 @@ public class BaseActivity extends Activity {
         return true;
     }
 
+    public void openCreateNewPlan() {
+        Intent intent = new Intent(getApplicationContext(), NewPlan.class);
+        startActivity(intent);
+    }
+
+    public void openClientInformation() {
+        Intent intent = new Intent(getApplicationContext(), ClientInformation.class);
+        startActivity(intent);
+    }
+
+    public void openBusinessInformation() {
+        Intent intent = new Intent(getApplicationContext(), BusinessInformation.class);
+        startActivity(intent);
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+        switch(item.getItemId()) {
+            case R.id.create_new_plan:
+                Log.i(TAG, "create_new_plan Item clicked");
+                openCreateNewPlan();
+                return true;
+
+            case R.id.client_information:
+                Log.i(TAG, "client_information Item Clicked");
+                openClientInformation();
+                return true;
+
+            case R.id.business_information:
+                Log.i(TAG, "business_information Item Clicked");
+                openBusinessInformation();
+                return true;
+
+
+
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }}
     }
-}
