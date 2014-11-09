c0e4f31b1584cf707009bf7cbb927bb1cf1a2f47
Florence <fswlee@gmail.com>
2014-11-08 19:53:54 -0500

    Implemented alertDialog on ActionPlan page
    done by Florence, Jackie, Hannah!

--- BusinessPlan/app/src/main/java/com/edu/umich/businessplan/businessplan/ActionPlan.java
+++ BusinessPlan/app/src/main/java/com/edu/umich/businessplan/businessplan/ActionPlan.java
@@ -8,14 +8,24 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.app.FragmentManager;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.view.View.OnClickListener;
+import android.content.Context;
 
 
 public class ActionPlan extends BaseActivity {
+
+
+    final Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_action_plan);
+
+
     }
 
 
@@ -28,9 +38,49 @@ public class ActionPlan extends BaseActivity {
     //onClick of DONE button?
     //Where will this take the user? How do we close app?
     public void openNewPlan(View view) {
-        Intent intent = new Intent(getApplicationContext(), NewPlan.class);
-        startActivity(intent);
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                context);
+
+        // set title
+        alertDialogBuilder.setTitle("GO TEAM BUSINESS PLAN");
+
+        // set dialog message
+        alertDialogBuilder
+                .setMessage("Click yes to exit!")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // if this button is clicked, close
+                        // current activity
+                        ActionPlan.this.openCreateNewPlan();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // if this button is clicked, just close
+                        // the dialog box and do nothing
+                        dialog.cancel();
+                    }
+                });
+
+        // create alert dialog
+        AlertDialog alertDialog = alertDialogBuilder.create();
+
+        // show it
+        alertDialog.show();
     }
+
+
+//        FragmentManager fm = getFragmentManager();
+//        TestDialog testDialog = new TestDialog();
+//        testDialog.setRetainInstance(true);
+//        testDialog.show(fm, "fragment_name");
+////        Intent intent = new Intent(getApplicationContext(), NewPlan.class);
+////        startActivity(intent);
+//    }
+
+
 
 
     @Override
