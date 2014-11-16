e6ff136da778ecf0ae0d92a930cec0a190250b06
HBrookhart <brookhar@msu.edu>
2014-11-15 20:27:12 -0500

    deleted pseudo code in YourCustomers
    the pseudo code has been implemented as real code! I'm deleting it
    because it takes up a ton of space.

--- BusinessPlan/app/src/main/java/com/edu/umich/businessplan/businessplan/YourCustomers.java
+++ BusinessPlan/app/src/main/java/com/edu/umich/businessplan/businessplan/YourCustomers.java
@@ -19,63 +19,6 @@ import java.util.List;
 
 public class YourCustomers extends BaseActivity {
 
-    // display the main menu button.
-    // display the name of the page.
-    // display first Q (where do you sell).
-    // display 4 buttons with images in table view
-    // display second Q (who do you sell to).
-    // display 4 buttons with images in table view
-    
-    // If a user selects a button in the first table, change 
-    // the state of that button from unselected to selected.
-    // User should be able to select as many as they want.
-
-        //Each button is it's own variable. e.g. location_home, location_neighborhood, location_store
-        //location_cart
-
-        //If selected, that variable =1, if not, variable=0
-    
-    // If a user selects a button in the second table, change 
-    // the state of that button from unselected to selected.
-    // User should be able to select as many as they want.
-
-        //each button is it's own variable, e.g. customer_family, customer_friends, customer_neighbors
-        //customer_public
-
-        //if selected, that variable =1, if not, variable=0
-        //Suggestions for Improvement for Business plan Business plan
-            //if customer_family =0, append  "family" to array rec_customer
-            //if customer_friends=0, append  "friends" to array rec_customer
-            //if customer_neighbors=0, append "neighbors" to array rec_customer
-            //if customer_public=0, append "public" to array rec_customer
-
-    // When the user selects "button1" (next), an onClick event
-    // is triggered.
-
-    
-    	// Update the location variables in the algorithm
-    	// that generates the recommendations to include the selected
-    	// options.
-    
-        // Update the customer variables in the algorithm
-        // that generates the recommendations to include the selected
-        // options.
-    
-    	// Load the previous activity (BusinessInformation).
-
-    // When the user selects "button2" (previous), an onClick event
-    // is triggered. 
-    
-    	// Update the location variables in the algorithm
-    	// that generates the recommendations to include the selected
-    	// options.
-    
-        // Update the customer variables in the algorithm
-        // that generates the recommendations to include the selected
-        // options.
-    
-    	// Load the next activity (YourHours).
-	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
