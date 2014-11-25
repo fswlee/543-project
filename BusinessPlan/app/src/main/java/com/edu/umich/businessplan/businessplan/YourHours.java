f58e9df112b7aca66a8d6f95453a62eb3c4e433a
HBrookhart <brookhar@msu.edu>
2014-11-25 12:51:56 -0500

    added onclick method selectHours() to YourHours
    implemented for morning buttons only

--- BusinessPlan/app/src/main/java/com/edu/umich/businessplan/businessplan/YourHours.java
+++ BusinessPlan/app/src/main/java/com/edu/umich/businessplan/businessplan/YourHours.java
@@ -13,6 +13,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
+import java.util.ArrayList;
+import java.util.List;
+
 //display textViews and Buttons upon activity instantiation
 
 //if the user selects any, button change its state from unselected to selected. the user can select
@@ -89,10 +92,18 @@ public class YourHours extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_your_hours);
+
+        calculateRecommendations();
     }
 
     //initialize selected status variables (bool)
     boolean selectedMondayMorning = false;
+    boolean selectedTuesdayMorning = false;
+    boolean selectedWednesdayMorning = false;
+    boolean selectedThursdayMorning = false;
+    boolean selectedFridayMorning = false;
+    boolean selectedSaturdayMorning = false;
+    boolean selectedSundayMorning = false;
 
 
 
@@ -104,10 +115,15 @@ public class YourHours extends BaseActivity {
     int eveningTotal = 0;
 
 
-    public void selectCustomers(View view) {
+    public void selectHours(View view) {
         //create variables for each button
         Button mondayMorning = (Button) findViewById(R.id.mondayMorning);
-
+        Button tuesdayMorning = (Button) findViewById(R.id.mondayMorning);
+        Button wednesdayMorning = (Button) findViewById(R.id.mondayMorning);
+        Button thursdayMorning = (Button) findViewById(R.id.mondayMorning);
+        Button fridayMorning = (Button) findViewById(R.id.mondayMorning);
+        Button saturdayMorning = (Button) findViewById(R.id.mondayMorning);
+        Button sundayMorning = (Button) findViewById(R.id.mondayMorning);
 
         //for each button
         //if it has already been selected
@@ -130,6 +146,129 @@ public class YourHours extends BaseActivity {
                 morningTotal -= 1;
             }
         }
+
+        if (view.getId() == R.id.tuesdayMorning) {
+            if (selectedTuesdayMorning == false) {
+                tuesdayMorning.setBackgroundColor(Color.BLACK);
+                selectedTuesdayMorning = true;
+                weekdayTotal += 1;
+                morningTotal += 1;
+            } else {
+                tuesdayMorning.setBackgroundColor(Color.rgb(45, 196, 137));
+                selectedTuesdayMorning = false;
+                weekdayTotal -= 1;
+                morningTotal -= 1;
+            }
+        }
+
+        if (view.getId() == R.id.wednesdayMorning) {
+            if (selectedWednesdayMorning == false) {
+                wednesdayMorning.setBackgroundColor(Color.BLACK);
+                selectedWednesdayMorning = true;
+                weekdayTotal += 1;
+                morningTotal += 1;
+            } else {
+                wednesdayMorning.setBackgroundColor(Color.rgb(45, 196, 137));
+                selectedWednesdayMorning = false;
+                weekdayTotal -= 1;
+                morningTotal -= 1;
+            }
+        }
+
+        if (view.getId() == R.id.thursdayMorning) {
+            if (selectedThursdayMorning == false) {
+                thursdayMorning.setBackgroundColor(Color.BLACK);
+                selectedThursdayMorning = true;
+                weekdayTotal += 1;
+                morningTotal += 1;
+            } else {
+                thursdayMorning.setBackgroundColor(Color.rgb(45, 196, 137));
+                selectedThursdayMorning = false;
+                weekdayTotal -= 1;
+                morningTotal -= 1;
+            }
+        }
+        if (view.getId() == R.id.fridayMorning) {
+            if (selectedFridayMorning == false) {
+                fridayMorning.setBackgroundColor(Color.BLACK);
+                selectedFridayMorning = true;
+                weekdayTotal += 1;
+                morningTotal += 1;
+            } else {
+                fridayMorning.setBackgroundColor(Color.rgb(45, 196, 137));
+                selectedFridayMorning = false;
+                weekdayTotal -= 1;
+                morningTotal -= 1;
+            }
+        }
+
+        if (view.getId() == R.id.saturdayMorning) {
+            if (selectedSaturdayMorning == false) {
+                saturdayMorning.setBackgroundColor(Color.BLACK);
+                selectedSaturdayMorning = true;
+                weekendTotal += 1;
+                morningTotal += 1;
+            } else {
+                saturdayMorning.setBackgroundColor(Color.rgb(45, 196, 137));
+                selectedSaturdayMorning = false;
+                weekendTotal -= 1;
+                morningTotal -= 1;
+            }
+        }
+
+        if (view.getId() == R.id.sundayMorning) {
+            if (selectedSundayMorning == false) {
+                sundayMorning.setBackgroundColor(Color.BLACK);
+                selectedSundayMorning = true;
+                weekendTotal += 1;
+                morningTotal += 1;
+            } else {
+                sundayMorning.setBackgroundColor(Color.rgb(45, 196, 137));
+                selectedSundayMorning = false;
+                weekendTotal -= 1;
+                morningTotal -= 1;
+            }
+        }
+    }
+
+    public void calculateRecommendations() {
+        List<String> suggestionList = new ArrayList<String>(); //empty list
+
+        //if fewer than two buttons from the group are selected
+        //add suggestions only if the button has not been selected
+        if (weekdayTotal<= 3) {
+            String suggestion = "Try selling more during the week";
+            suggestionList.add(suggestion);
+        }
+        if (weekendTotal == 0) {
+            String suggestion = "Try selling on the weekend";
+            suggestionList.add(suggestion);
+        }
+        if (morningTotal <= 3) {
+            String suggestion = "Try selling more during the morning";
+            suggestionList.add(suggestion);
+        }
+        if (afternoonTotal <= 3) {
+            String suggestion = "Try selling more during the afternoon";
+            suggestionList.add(suggestion);
+        }
+        if (eveningTotal <= 3) {
+            String suggestion = "Try selling more during the evening";
+            suggestionList.add(suggestion);
+        }
+
+
+
+
+        Log.i("MyActivity", "suggestionList " + suggestionList);
+
+        //add the suggestionList to the sharedPreferences using
+        //  the method in the SharedPreferences Utility class
+        //TODO grab shared prefs from previous screens in order to add these recommendations
+        SharedPreferencesUtility.putStringList(this, "recommendation", suggestionList);
+        //use the lines below to test whether the correct suggestions were added
+        suggestionList = SharedPreferencesUtility.getStringList(this, "recommendation");
+        Log.i("MyActivity", "sharedPreferences " + suggestionList);
 
     }
 
