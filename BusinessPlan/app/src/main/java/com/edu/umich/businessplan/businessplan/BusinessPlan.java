d5560203e44487eec5849b8b78ea403b702a3727
HBrookhart <brookhar@msu.edu>
2014-12-07 21:07:33 -0500

    created new class, BusinessPlan.java
    each new user creates a new BusinessPlan object
    attributes:
    String name
    String city
    Integer income
    List suggestions
    List actions

---
+++ BusinessPlan/app/src/main/java/com/edu/umich/businessplan/businessplan/BusinessPlan.java
@@ -0,0 +1,88 @@
+package com.edu.umich.businessplan.businessplan;
+
+import java.util.List;
+
+/**
+ * Created by Hannah Brookhart on 12/7/2014.
+ */
+
+
+//for shared preferences: only one key the name, access other prefs via index
+    //"client name": city (0), income (1), suggestionsList (2), actionsList (3)
+    //shared prefs: key is a string, client name
+    //value is a list with 4 elements: [String city, int income, List suggestionsList(list of Suggestions), List actionsList(Strings)
+public class BusinessPlan {
+//a business plan is generated for each user
+    String name;
+    String city;
+    Integer income;
+    List suggestions;
+    List actions;
+
+    //constructor creates BusinessPlan object using name and city
+        //also includes default income, suggestions, and actions (all empty)
+    public BusinessPlan(String name) {
+        super();
+        this.name = name;
+        this.city = null;
+        this.income = null;
+        this.suggestions = null;
+        this.actions = null;
+    }
+
+    //there is no set for name or because this is set using the constructor and should not
+    //be changed
+
+    public String getName() {
+        //gets the String name
+        return name;
+    }
+
+    public void setCity(String clientCity) {
+        //sets the String city (in CLientInformation.java)
+        city = clientCity;
+    }
+
+
+    public String getCity() {
+        //gets the String city
+        return city;
+    }
+
+    public void setIncome(int clientIncome) {
+        //sets the income (used in BusinessInformation.java)
+        income = clientIncome;
+    }
+
+    public Integer getIncome() {
+        //gets the income (used in ActionPlan.java and YouAndYourCommunity.java)
+        return income;
+    }
+
+    public void addSuggestions(Suggestion suggestion) {
+        //adds a suggestion to the list of suggestions (class Suggestion(String, Boolean)
+        //these are added on YourCustomers and YourHours
+        suggestions.add(suggestion);
+    }
+
+    public void removeSuggestions(Suggestion suggestion) {
+        //removes a suggestion from the list of suggestions (class Suggestion(String, Boolean)
+        suggestions.remove(suggestion);
+    }
+
+    public List getSuggestions() {
+        //returns a list of Suggestions (String,Boolean)
+        return suggestions;
+    }
+
+    public void addActions(String action) {
+        //adds an action to the list of actions
+        //actions are added on SuggestionsForImprovement.java when a user selects a checkbox
+        actions.add(action);
+    }
+
+    public List getActions() {
+        //returns a list of actions(Strings)
+        return actions;
+    }
+}
