609ab2254632287cf3ae498982b19a8cb912ff5d
Florence <fswlee@gmail.com>
2014-12-10 07:17:28 -0500

    remove extraneous teamivore code

--- BusinessPlan/app/src/main/java/com/edu/umich/businessplan/businessplan/PlansOverview.java
+++ BusinessPlan/app/src/main/java/com/edu/umich/businessplan/businessplan/PlansOverview.java
@@ -28,7 +28,7 @@ import java.util.Map;
 public class PlansOverview extends BaseActivity {
 
     // the string variable we use for sending messages with intents
-    public final static String EXTRA_MESSAGE = "edu.umich.teamivore.MESSAGE";
+//    public final static String EXTRA_MESSAGE = "edu.umich.teamivore.MESSAGE";
 
     MyCustomAdapter dataAdapter = null;
 
